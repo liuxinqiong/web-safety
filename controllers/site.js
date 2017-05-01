ec042e7b0349c8dcfcd25887736bf703474caea4
TooBug <i@toobug.net>
2017-05-01 18:58:51 +0800

    美化界面

--- controllers/site.js
+++ controllers/site.js
@@ -4,10 +4,13 @@ const connectionModel = require('../models/connection');
 exports.index = async function(ctx, next){
 	const connection = connectionModel.getConnection();
 	const query = bluebird.promisify(connection.query.bind(connection));
-	const results = await query(
-			'select * from post limit 10'
+	const posts = await query(
+			'select post.*,count(comment.id) as commentCount from post left join comment on post.id = comment.postId group by post.id limit 10'
 		);
-	ctx.render('index', {posts: results, from:ctx.query.from || ''});
+	const comments = await query(
+			'select comment.*,post.id as postId,post.title as postTitle,user.username as username from comment left join post on comment.postId = post.id left join user on comment.userId = user.id order by comment.id desc limit 10'
+		);
+	ctx.render('index', {posts, comments, from:ctx.query.from || ''});
 	connection.end();
 };
 
