118e0a7b096fa837480c5ed9afad986ef52cc784
TooBug <i@toobug.net>
2017-05-18 07:45:52 +0800

    点击支持X-FRAME-OPTIONS

--- routes/site.js
+++ routes/site.js
@@ -9,6 +9,7 @@ const captcha = require('../tools/captcha');
 router.all('/*', async function(ctx, next){
 	console.log('enter site.js');
 	ctx.set('X-XSS-Protection', 0);
+	ctx.set('X-Frame-Options', 'DENY');
 	// ctx.set(`Content-Security-Policy`, `script-src 'self' 'sha256-nIk3U+a69DO8a0GCyK2V/qcQHQ1Y/hkEZjg9CpNxEgQ='`)
 	await next();
 });
